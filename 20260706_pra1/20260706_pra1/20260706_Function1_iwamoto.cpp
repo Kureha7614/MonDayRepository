baedd4fd9047931203bb636982bf6faa91b2d2f2
Kureha7614 <laixiyanben@gmail.com>
2026-07-06 10:47:02 +0900

    練習問題１

---
+++ 20260706_pra1/20260706_pra1/20260706_Function1_iwamoto.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include"20260706_Header1_iwamoto.h"
+using namespace std;
+
+int InputChoise(int min,int max)
+{
+	//変数
+	int choice;
+	//入力チェック
+	while (true)
+	{
+		//０か1を入力させる
+		cin >> choice;
+		if (choice >= min && choice <= max)
+		{
+			break;
+		}
+		else
+		{
+			cout << min << "から" << max << "を入力してください" << endl;
+		}
+	}
+	return choice;
+}
+
+
+void Heal(int& Hp)
+{
+	//変数
+	int choice;
+
+	cout << "回復しますか？？（Yes：1、No：0）" << endl;
+	choice = InputChoise(Min, Max);
+	if (choice == 1)
+	{
+		//回復
+		Hp += 20;
+		cout << "HPを２０回復しました" << endl;
+	}
+	else
+	{
+		cout << "回復しませんでした" << endl;
+	}
+}
+
+void Game()
+{
+	//変数
+	int playerHp = 0;
+	//乱数の初期化
+	srand((unsigned int)time(NULL));
+
+	cout << "プレイヤーのHPは100です。" << endl;
+
+	//ランダムにプレイヤーのHPを設定する
+	playerHp = rand() % 101;
+
+	//プレイヤーのHPを表示する
+	cout << "プレイヤーのHPは" << playerHp << "です。" << endl;
+
+	//プレイヤーのHPを回復する or しない
+	Heal(playerHp);
+
+	//プレイヤーのHPを表示する
+	cout << "プレイヤーのHPは" << playerHp << "です。" << endl;
+
+}
---
+++ 20260706_pra1/20260706_pra1/20260706_Header1_iwamoto.h
@@ -0,0 +1,10 @@
+#pragma once
+const int Max = 1;
+const int Min = 0;
+const int PlayerMaxHp = 100;
+const int HealAmount = 20;
+
+//関数のプロトタイプ宣言
+int InputChoise(int min, int max);
+void Heal(int& Hp);
+void Game();
